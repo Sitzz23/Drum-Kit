1c8297deefdedf5c1556ce25764f1355b608e4a9
Sitanshu Pokalwar <sitz2309@gmail.com>
2022-01-07 03:07:31 +0530

    ✨

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-function playSound(e){
-    const beat = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    beat.currentTime = 0;
-    beat.play();
+function playSound(e) {
+  const beat = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+  if (!beat) return;
+  key.classList.add("currentlyPlaying");
+  beat.currentTime = 0;
+  beat.play();
 }
 
-window.addEventListener('keydown', playSound);
+window.addEventListener("keydown", playSound);
+const keys = Array.from(document.querySelectorAll(".key"));
+keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
+
+function removeTransition(e) {
+  if (e.propertyName !== "transform") return;
+  e.target.classList.remove("currentlyPlaying");
+}
